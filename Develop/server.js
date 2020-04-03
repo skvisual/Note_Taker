c1442ecde75620e1c555b469138a90d985d403c1
skvisual <southkvisual@gmail.com>
2020-04-03 07:03:18 -0500

    Progress on api/notes POST

--- Develop/server.js
+++ Develop/server.js
@@ -3,11 +3,10 @@ var http = require("http");
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
-// var data = fs.readFileSync('./db/db.json');
-// var notesDB = require('./db/db.json');
+var notesDB = require('./db/db.json');
 // Instantiate a new express app utilizing the express() method.
 var app = express();
-const notesDB = [];
+// const notesDB = [];
 
 //Decleare a port number for the server to find the application.
 var PORT = 8080;
@@ -17,8 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-//  Create .get routes
-    
+//////////////////////// HTML ROUTES ////////////////////////////   
     // Return the index page
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
@@ -37,24 +35,23 @@ app.get("*", function(req, res) {
     //set up the notes
 app.get("/api/notes", function(req, res) {
     //response to access notesDB variable in order to send to response.
-    res.json(notesDB);
+    res.json(db.json);
 });
 
 // Create the API POST routes
 app.post('/api/notes', function(req, res) {
-    
-  var newNote = req.body;
+   
+  //assigns req.body to a variable
+  const newNote = req.body;
 
-//   console.log(req.body)
+  notesDB.push(newNote);
 
-
-  // notesDB.push(newNote);
-  fs.appendFileSync('./db/db.json', JSON.stringify([newNote]));
-
-  console.log(newNote)
-
-
-  // res.json(newNote);
+  fs.writeFile('./db/db.json', JSON.stringify(notesDB, null, 2), (err) => {
+    if (err){
+      console.log('ERROR: Could not add note to database!')
+    }
+  });
+  res.json(notesDB);
 });
 
 // Create the API delete routes
